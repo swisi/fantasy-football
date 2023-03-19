8ab4ce5a3f3c58db94e600a028f7f2218246f9c4
swisi <swisi71@gmail.com>
2023-03-19 21:34:12 +0100

    added ALLOWED_HOSTS = ['167.235.226.17']

--- fantasyfootball/settings.py
+++ fantasyfootball/settings.py
@@ -123,6 +123,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = ['167.235.226.17']
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
