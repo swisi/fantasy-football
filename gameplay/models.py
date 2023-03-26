d58cffb9769a3d19613ef7f1d276bfcf2df3a185
swisi <swisi71@gmail.com>
2023-03-26 13:00:53 +0200

    Version vor Gameplay Formular

--- gameplay/models.py
+++ gameplay/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from nfldata.models import NFL_Players
 
-class GameWeek(models.Model):
-    week_number = models.IntegerField(unique=True)
-    qb = models.ManyToManyField(NFL_Players, related_name='qb_selected', blank=True)
-    rb = models.ManyToManyField(NFL_Players, related_name='rb_selected', blank=True)
-    wr = models.ManyToManyField(NFL_Players, related_name='wr_selected', blank=True)
-    te = models.ManyToManyField(NFL_Players, related_name='te_selected', blank=True)
-    k = models.ManyToManyField(NFL_Players, related_name='k_selected', blank=True)
-    qb_backup = models.ManyToManyField(NFL_Players, related_name='qb_backup_selected', blank=True)
-    rb_backup = models.ManyToManyField(NFL_Players, related_name='rb_backup_selected', blank=True)
-    wr_backup = models.ManyToManyField(NFL_Players, related_name='wr_backup_selected', blank=True)
-    te_backup = models.ManyToManyField(NFL_Players, related_name='te_backup_selected', blank=True)
-    k_backup = models.ManyToManyField(NFL_Players, related_name='k_backup_selected', blank=True)
+class GamePlay(models.Model):
+    week_number = models.IntegerField()
+    qb = models.ForeignKey(NFL_Players, related_name='qb_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    rb = models.ForeignKey(NFL_Players, related_name='rb_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    wr = models.ForeignKey(NFL_Players, related_name='wr_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    te = models.ForeignKey(NFL_Players, related_name='te_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    k = models.ForeignKey(NFL_Players, related_name='k_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    qb_backup = models.ForeignKey(NFL_Players, related_name='qb_backup_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    rb_backup = models.ForeignKey(NFL_Players, related_name='rb_backup_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    wr_backup = models.ForeignKey(NFL_Players, related_name='wr_backup_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    te_backup = models.ForeignKey(NFL_Players, related_name='te_backup_selected', on_delete=models.SET_NULL, null=True, blank=True)
+    k_backup = models.ForeignKey(NFL_Players, related_name='k_backup_selected', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
+        unique_together = ('week_number',)
         ordering = ['week_number']
+
