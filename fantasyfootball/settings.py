cc6fea38149deed7f397f489b84e8e8f9fe8c6c4
swisi <swisi71@gmail.com>
2023-03-26 13:55:18 +0200

    Middleware für User erstellt!

--- fantasyfootball/settings.py
+++ fantasyfootball/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'fantasyfootball.middlewares.user.UserMiddleware',
+
 ]
 
 ROOT_URLCONF = 'fantasyfootball.urls'
