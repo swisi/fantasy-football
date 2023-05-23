c239ce1b0b6858eb975567c7856ff71f2ee5fa06
swisi <swisi71@gmail.com>
2023-05-23 17:05:01 +0200

    Fixed error

--- temp/test01.py
+++ temp/test01.py
@@ -1,10 +1,10 @@
-import pandas as pd
 import nfl_data_py as nfl
 
 
-years = [2022]  # Change this to use other years if needed
-columns_R = ['player_id', 'position', 'team', 'player_name', 'jersey_number', 'status']
-     
-dfp = nfl.import_rosters(years, columns_R)
 
-print(dfp)
+years = [2022]  # Specify the year of the season you want to retrieve information about
+
+
+season_info = nfl.import_seasonal_data(years)
+
+printU(season_info.head())
